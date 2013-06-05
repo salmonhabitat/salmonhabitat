7333b5b0df05ccbc02384d8b5f88c7fd67c52442
jacquestardie <jacquestardie@gmail.com>
2013-06-05 19:05:38 -0400

    the beginnings of an API

---
+++ routes/api.js
@@ -0,0 +1,31 @@
+module.exports = function(app) {
+
+  // GET all events
+  app.get('/api/events', function(req, res) {
+    events.find().toArray(function(err, result) { 
+      res.send(result); 
+    }) 
+  });
+
+  // GET single event
+  app.get('/api/events/:name', function(req, res) {
+    events.find({ title: req.params.name }).toArray(function(err, result) {
+      if (err) throw err;
+      res.json(result);
+    })
+  });
+
+  // POST single event
+  // DELETE single event
+
+  // GET all projects
+
+  var base        = 'http://jacques.cartodb.com/api/v2/sql?q=SELECT%20*%20FROM%20';
+
+  app.get('/api/projects/:type', function(req, res) { res.redirect(base + req.params.type) });
+  app.get('/api/projects/:type/download', function(req, res) { res.redirect(base + req.params.type + '&format=SHP') });
+  
+
+}
+
+var events = require('mongoskin').db('mongodb://jacques:momaddie@linus.mongohq.com:10037/share').collection('events');
