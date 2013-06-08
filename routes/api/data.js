cc9af403c6a3839ed75b026d46d582f97b9a0cf8
jacquestardie <jacquestardie@gmail.com>
2013-06-08 07:31:14 -0400

    Using cartodb-node

--- routes/api/data.js
+++ routes/api/data.js
@@ -1,12 +1,31 @@
 module.exports = function(app) {
 
-  app.get('/api/projects/:type', function(req, res) { res.redirect(base + req.params.type) });
-  app.get('/api/projects/:type/download', function(req, res) { res.redirect(base + req.params.type + '&format=SHP') });
-  
+  // GET data, JSON representation
+  app.get('/api/data/:type', function(req, res) {
+
+    var client = new carto({
+      user:     credentials.cartoUser,
+      api_key:  credentials.cartoKey
+    });
+
+    var output = function(err, data) {
+      res.send(data.rows);
+    };
+
+    client.on('connect', function() {
+      client.query('select * from ' + req.params.type, output);
+    });
+
+    client.connect();
+
+  });
+
+
+  // Download data as a shapefile
+  app.get('/api/data/:type/download', function(req, res) { res.redirect(base + req.params.type + '&format=SHP') });
 
 }
 
 var carto       = require('cartodb'),
-    credentials = require('../credentials');
-
-var base        = 'http://jacques.cartodb.com/api/v2/sql?q=SELECT%20*%20FROM%20';
+    credentials = require('../../credentials'),
+    base        = 'http://jacques.cartodb.com/api/v2/sql?q=SELECT%20*%20FROM%20';
